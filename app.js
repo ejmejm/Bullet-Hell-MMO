f63f04f22c2d39740fff40de009534e4761b2e2f
Edan Meyer <ninjaedan@aol.com>
2016-05-25 21:07:58 -0500

    First Commit

---
+++ app.js
@@ -0,0 +1,84 @@
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/client/index.html');
+});
+app.use('/client', express.static(__dirname + '/client'));
+
+var port = Number(process.env.PORT || 2000);
+
+serv.listen(port);
+console.log('Server Started');
+
+var SOCKET_LIST = {};
+var PLAYER_LIST = {};
+
+var Player = function(id){
+	var self = {
+		x: 0,
+		y: 0,
+		speed: 3,
+		up: false,
+		down: false,
+		left: false,
+		right: false,
+		id: id
+	}
+	
+	self.updatePosition = function(){
+		if(self.up)
+			self.y -= self.speed;
+		if(self.down)
+			self.y += self.speed;
+		if(self.left)
+			self.x -= self.speed;
+		if(self.right)
+			self.x += self.speed;
+	}
+	
+	return self;
+}
+
+var io = require('socket.io')(serv, {});
+io.sockets.on('connection', function(socket){
+	socket.id = Math.random();
+	SOCKET_LIST[socket.id] = socket;
+	
+	var player = Player(socket.id);
+	PLAYER_LIST[socket.id] = player;
+	
+	socket.on('disconnect', function(){
+		delete SOCKET_LIST[socket.id];
+		delete PLAYER_LIST[socket.id];
+	});
+	
+	socket.on('keyPress', function(data){
+		if(data.inputId === 'up')
+			player.up = data.state;
+		else if(data.inputId === 'down')
+			player.down = data.state;
+		else if(data.inputId === 'left')
+			player.left = data.state;
+		else if(data.inputId === 'right')
+			player.right = data.state;
+	});
+});
+
+setInterval(function(){
+	var pack = [];
+	for(var i in PLAYER_LIST){
+		var player = PLAYER_LIST[i];
+		player.updatePosition();
+		pack.push({
+			x: player.x,
+			y: player.y
+		});
+	}
+	
+	for(var i in SOCKET_LIST){
+		var socket = SOCKET_LIST[i];
+		socket.emit('positionUpdate', pack);
+	}
+}, 1000/40);
